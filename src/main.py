bc72e0ec46bf120cd6bce37fa3a8abc12c8fdc45
nighthawk469 <nick.crawford1234@gmail.com>
2017-02-11 17:20:20 -0500

    fixes

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-"""Reads and writes serial data from an attached arduino.
+"""Reads serial data from ardiuno, send data with timestamp to plotly graph.
 
 The data is given with a timestamp from python for graphing purposes.
 
@@ -22,7 +22,7 @@ import plotly.plotly as py
 from plotly_graph import PlotlyGraph
 
 logging.basicConfig(level=logging.DEBUG,
-                    filename='errors.log',
+                    filename='/home/pi/herbGarden/src/errors.log',
                     format = '%(asctime)s %(message)s')
 
 #fixes IOError: [Errno 32] Broken pipe
